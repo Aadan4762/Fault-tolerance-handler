7c00604510ecfc56012fd16a73ab73026ef6a02f
adesh <aadan4762@gmail.com>
2023-10-21 17:58:46 +0300

    initial commit

---
+++ ServiceB/src/test/java/com/adan/ServiceB/ServiceBApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adan.ServiceB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceBApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
